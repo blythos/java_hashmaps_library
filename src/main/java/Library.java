0db72132a9e08d807e59f4369a3aa5170347f1c6
Stephen Blythe <stephenblythe@gmail.com>
2019-09-24 16:11:56 +0100

    Tests if library has books

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> collection;
+    private int capacity;
+
+    public Library(int capacity){
+        this.collection = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
+
+    public void addBook(Book book) {
+        this.collection.add(book);
+    }
+
+    public int checkCollectionSize(){
+        return this.collection.size();
+    }
 }
---
+++ src/test/java/TestLibrary.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestLibrary {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void before(){
+        library = new Library(5);
+        book1 = new Book("Equal Rites", "Terry Pratchett", "Fantasy");
+        book2 = new Book("The Traitor Baru Cormorant", "Seth Dickinson", "Fantasy");
+        book3 = new Book("My Year of Rest and Relaxation", "Ottessa Moshfegh", "Literary");
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+    }
+
+    @Test
+    public void libraryHasBooks(){
+        assertEquals(3, library.checkCollectionSize());
+    }
+}
